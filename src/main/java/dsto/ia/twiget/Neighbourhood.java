a340b012d06bbfdd408e006ee672e7c86aa4e3db
Derek Weber <derek.weber@dsto.defence.gov.au>
2015-03-17 09:41:00 +1030

    Minor.

--- src/main/java/dsto/ia/twiget/Neighbourhood.java
+++ src/main/java/dsto/ia/twiget/Neighbourhood.java
@@ -1,18 +1,31 @@
 package dsto.ia.twiget;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
-import twitter4j.User;
-
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+
+import twitter4j.User;
 
 public class Neighbourhood
 {
   public User profile;
   public final Set<Long> followerIDs = Sets.newHashSet ();
   public final Set<Long> followeeIDs = Sets.newHashSet ();
+
+  public Neighbourhood ()
+  {
+    // do nothing contructor
+  }
+
+  public Neighbourhood (User profile, Collection<Long> followeeIDs, Collection<Long> followerIDs)
+  {
+    this.profile = profile;
+    this.followeeIDs.addAll (followeeIDs);
+    this.followerIDs.addAll (followerIDs);
+  }
 
   public Map<String, Object> getSnapshot ()
   {
